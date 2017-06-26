e6ab9c2ef1c173d593ccee60080dc310b3079ff0
DESKTOP-LJ5ARQU\saite <saiteja.saiteja54@gmail.com>
2017-06-26 16:05:43 -0400

    Sample Angular App

---
+++ quiz.js
@@ -0,0 +1,10 @@
+(function () {
+      var map = angular.module("quizApp",[]);
+      map.controller("quizCtrl", QuizController);
+
+      function QuizController() {
+        var vm = this;
+      }
+
+
+    })();
